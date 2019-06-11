375e9a3f8f292727dab323fdc95b2764fddd3297
kcreddyv <31012572+kcreddyv@users.noreply.github.com>
2019-06-10 22:40:09 -0700

    Update DBConnectionUtility.java

--- src/main/java/com/app/util/DBConnectionUtility.java
+++ src/main/java/com/app/util/DBConnectionUtility.java
@@ -4,23 +4,25 @@ import java.sql.*;
 import javax.naming.*;
 import javax.sql.*;
 
-private static Connection getRemoteConnection() {
-    if (System.getProperty("RDS_HOSTNAME") != null) {
-      try {
-      Class.forName("org.postgresql.Driver");
-      String dbName = System.getProperty("RDS_DB_NAME");
-      String userName = System.getProperty("RDS_USERNAME");
-      String password = System.getProperty("RDS_PASSWORD");
-      String hostname = System.getProperty("RDS_HOSTNAME");
-      String port = System.getProperty("RDS_PORT");
-      String jdbcUrl = "jdbc:postgresql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
-      logger.trace("Getting remote connection with connection string from environment variables.");
-      Connection con = DriverManager.getConnection(jdbcUrl);
-      logger.info("Remote connection successful.");
-      return con;
+public static DBConnectionUtility{
+    public static Connection getRemoteConnection() {
+        if (System.getProperty("RDS_HOSTNAME") != null) {
+        try {
+        Class.forName("org.postgresql.Driver");
+        String dbName = System.getProperty("RDS_DB_NAME");
+        String userName = System.getProperty("RDS_USERNAME");
+        String password = System.getProperty("RDS_PASSWORD");
+        String hostname = System.getProperty("RDS_HOSTNAME");
+        String port = System.getProperty("RDS_PORT");
+        String jdbcUrl = "jdbc:postgresql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
+        logger.trace("Getting remote connection with connection string from environment variables.");
+        Connection con = DriverManager.getConnection(jdbcUrl);
+        logger.info("Remote connection successful.");
+        return con;
+        }
+        catch (ClassNotFoundException e) { logger.warn(e.toString());}
+        catch (SQLException e) { logger.warn(e.toString());}
+        }
+        return null;
     }
-    catch (ClassNotFoundException e) { logger.warn(e.toString());}
-    catch (SQLException e) { logger.warn(e.toString());}
-    }
-    return null;
-  }
+}
